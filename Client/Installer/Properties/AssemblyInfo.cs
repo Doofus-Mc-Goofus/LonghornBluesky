a3da4065b125525450f2e366bad31d6d8bfc616a
Doofus-Mc-Goofus <ericwilliam2904@gmail.com>
2025-07-18 02:56:21 -0400

    Tried to fix Windows Defender false positive

--- Client/Installer/Properties/AssemblyInfo.cs
+++ Client/Installer/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Longhorn Bluesky Setup")]
+[assembly: AssemblyTitle("Longhorn Bluesky")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("DoofusMcGoofus")]
